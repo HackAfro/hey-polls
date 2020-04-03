3bbc65562a93a4f6b1750ec5910ecb6f15aa305a
Richard Afro <richyafro@hotmail.com>
2020-04-03 16:59:16 +0100

    add routes and set modal element

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactModal from 'react-modal';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Routes from './router';
+
+ReactModal.setAppElement('#root');
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routes />
   </React.StrictMode>,
   document.getElementById('root')
 );
