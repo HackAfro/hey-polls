122e353d54645a0f44b8cc1adce08dc6a9d88f53
Richard Afro <richyafro@hotmail.com>
2020-04-03 17:00:31 +0100

    remove unused code

--- src/router.js
+++ src/router.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AppProvider from './providers';
 import App from './App';
-import { AppContext } from './contexts';
-import QuestionDetail from './components/questionDetail';
 
 const Routes = () => {
   return (
